744370f00340e41909e364c07c68217a8afc01b9
abishakeG-106 <98737728+abishakeG-106@users.noreply.github.com>
2022-08-21 15:20:17 -0400

    animation code js for button chover
    added animation code for button hover

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 
 console.log("Your index.js file is loaded correctly!");
 
+$("DownloadResume").on("click", function () {
+    
+    console.log("You clicked on #DownloadResume");
+});
+
+$("button").on("touchsart mousedown", function () {
+	$(this).addClass("clicked");
+});
+
+$("button").on("touchend mouseup", function () {
+	$(this).removeClass("clicked");
+	$(this).blur();
+});
+
